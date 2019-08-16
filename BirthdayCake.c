ed745d2195e4458e9d13f6b96c452a747386c2c8
VallamkondaNeelima <42175368+VallamkondaNeelima@users.noreply.github.com>
2019-08-16 23:03:08 +0530

    Create BirthdayCake.c

---
+++ BirthdayCake.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int i,count=0,j,n,m,d,a[101],sum;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	scanf("%d %d",&d,&m);
+	for(i=0;n-i>=m;i++)
+	{
+		sum=0;
+		for(j=i;j<i+m;j++)
+		{
+			sum=sum+a[j];
+		}
+		if(sum==d)
+		count++;
+		
+		
+	}
+	printf("%d",count);
+	return 0;
+}
